eed8df8bb0e265fe9a6ddcf91c7e2759227de714
ttancter1303 <01689674504zx@gmail.com>
2023-11-21 14:12:30 +0700

    ttan: fix data

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import org.example.data.XeMay;
 
 import java.util.Scanner;
 
--- src/main/java/org/example/Manager.java
+++ src/main/java/org/example/Manager.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
 
 public class Manager {
     String filePath = "DataPTGT.json";
-    ArrayList<Vehicle> vehicles = (ArrayList<Vehicle>) readData();
+    ArrayList<Vehicle> vehicles = new ArrayList<>();
     public void add_vehicles()
     {
         Scanner scanner = new Scanner(System.in);
@@ -33,8 +33,7 @@ public class Manager {
         int dung_tich = scanner.nextInt();
 
         scanner.nextLine();
-        System.out.println("nhập loại xe ");
-        String loai_xe = scanner.nextLine();
+        String loai_xe = "XeMay";
 
         System.out.print("Nhập giá tiền: ");
         double gia_tien = scanner.nextInt();
@@ -44,6 +43,7 @@ public class Manager {
 
         System.out.print("Nhập mã số thuế: ");
         String ma_so_thue = scanner.nextLine();
+        maSoThueChecker(vehicles,ma_so_thue);
 
         scanner.nextLine();
 
@@ -55,21 +55,21 @@ public class Manager {
         // thêm đối tượng xe máy vào danh sach vehicles \
         vehicles.add(xeMay);
         //  lưu danh sách vehicles vào tệp tin
-
+        saveData();
 
     }
     public void testData(){
         XeMay xeMay = new XeMay("XeMay","Wave","honda",2015,10007000,1.5F,"TN986876GV76","Ta Tuan Anh",100);
-        XeMay xeMay1 = new XeMay("XeMay1","Wave","honda",2015,10300000,1.5F,"TN9868764V56","Ta Tuan Anh",100);
-        XeMay xeMay2 = new XeMay("XeMay2","Wave","honda",2015,10800000,1.5F,"TN986873GV56","Ta Tuan Anh",100);
-        XeMay xeMay3 = new XeMay("XeMay3","Wave","honda",2015,10300000,1.5F,"TN986871GV56","Ta Tuan Anh",100);
-        XeMay xeMay4 = new XeMay("XeMay4","Wave","honda",2015,10008000,1.5F,"TN986879GV56","Ta Tuan Anh",100);
-        XeOtoCon otoCon0 = new XeOtoCon("Oto0","civic","honda",2019,190000800,2F,"BD98965896HY","Phung Ngoc Bao",1035,100);
-        XeOtoCon otoCon1 = new XeOtoCon("Oto1","civic","honda",2019,190400000,2F,"BD98795896HY","Phung Ngoc Bao",1035,100);
-        XeOtoCon otoCon2 = new XeOtoCon("Oto2","civic","honda",2019,190080000,2F,"BD98705896HY","Phung Ngoc Bao",1035,100);
-        XeOtoCon otoCon3 = new XeOtoCon("Oto3","civic","honda",2019,199000000,2F,"BD98715896HY","Phung Ngoc Bao",1035,100);
-        XeOtoCon otoCon4 = new XeOtoCon("Oto4","civic","honda",2019,190004000,2F,"BD987905896HY","Phung Ngoc Bao",1035,100);
-        XeOtoCon otoCon5 = new XeOtoCon("Oto5","civic","honda",2019,195000000,2F,"BD98764896HY","Phung Ngoc Bao",1035,100);
+        XeMay xeMay1 = new XeMay("XeMay","Wave","honda",2015,10300000,1.5F,"TN9868764V56","Ta Tuan Anh",100);
+        XeMay xeMay2 = new XeMay("XeMay","Wave","honda",2015,10800000,1.5F,"TN986873GV56","Ta Tuan Anh",100);
+        XeMay xeMay3 = new XeMay("XeMay","Wave","honda",2015,10300000,1.5F,"TN986871GV56","Ta Tuan Anh",100);
+        XeMay xeMay4 = new XeMay("XeMay","Wave","honda",2015,10008000,1.5F,"TN986879GV56","Ta Tuan Anh",100);
+        XeOtoCon otoCon0 = new XeOtoCon("Oto","civic","honda",2019,190000800,2F,"BD98965896HY","Phung Ngoc Bao",1035,100);
+        XeOtoCon otoCon1 = new XeOtoCon("Oto","civic","honda",2019,190400000,2F,"BD98795896HY","Phung Ngoc Bao",1035,100);
+        XeOtoCon otoCon2 = new XeOtoCon("Oto","civic","honda",2019,190080000,2F,"BD98705896HY","Phung Ngoc Bao",1035,100);
+        XeOtoCon otoCon3 = new XeOtoCon("Oto","civic","honda",2019,199000000,2F,"BD98715896HY","Phung Ngoc Bao",1035,100);
+        XeOtoCon otoCon4 = new XeOtoCon("Oto","civic","honda",2019,190004000,2F,"BD987905896HY","Phung Ngoc Bao",1035,100);
+        XeOtoCon otoCon5 = new XeOtoCon("Oto","civic","honda",2019,195000000,2F,"BD98764896HY","Phung Ngoc Bao",1035,100);
 
         vehicles.add(xeMay);
         vehicles.add(xeMay1);
